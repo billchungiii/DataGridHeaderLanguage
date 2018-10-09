50ec88f689a626df48929163b7937aaf312e5d2d
Bill Chung <bill_chungiii@hotmail.com>
2018-10-10 05:40:36 +0800

    加入專案檔案。

---
+++ ColumnBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace DataGridHeaderLanguage
+{
+    public class ColumnBehavior : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.AutoGeneratingColumn += AssociatedObject_AutoGeneratingColumn;
+        }
+
+        private void AssociatedObject_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            e.Column.Header = AppResource.ResourceManager.GetString(e.Column.Header.ToString());
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.AutoGeneratingColumn -= AssociatedObject_AutoGeneratingColumn;
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridHeaderLanguage
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class MainViewModel
+    {
+        public List<Person> People { get; set; }
+
+        public MainViewModel()
+        {
+            People = new List<Person>
+            {
+                new Person {Name = "Bill" , Age = 25},
+                new Person {Name = "David" , Age = 27}
+            };
+        }
+    }
+}
